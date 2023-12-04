4ecfc3f71f08d5dab8631fda39d22e54909552b1
MichaelJC10 <152749262+MichaelJC10@users.noreply.github.com>
2023-12-04 09:57:58 -0500

    Create Maquina_Principal.h

---
+++ Maquina_Principal.h
@@ -0,0 +1,10 @@
+#ifndef MAQUINA_PRINCIPAL_H
+#define MAQUINA_PRINCIPAL_H
+
+#include "Arduino.h"
+
+extern unsigned long tiempo;
+extern unsigned long tiempo2;
+
+void ejecucionTrayectoria(bool,bool);
+#endif

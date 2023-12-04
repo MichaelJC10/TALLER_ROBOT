0b4811e0364928c86b50db241f22db9d6188bd5b
MichaelJC10 <152749262+MichaelJC10@users.noreply.github.com>
2023-12-04 10:59:05 -0500

    Update Maquina_Principal.cpp

--- Maquina_Principal.cpp
+++ Maquina_Principal.cpp
@@ -3,109 +3,51 @@
 
 
 
-
-void estados( bool pelota) {
+void estados(bool pelota, bool destino) {
   // Verifica la presencia de la pelota
-  if (digitalRead(led) == LOW) {
-    parar
-    estado = 1;
-    
-    // Puedes agregar más lógica relacionada al caso 6 si es necesario
-  } else {
+  byte estado =0;
+ switch (estado){
+	case 0:
+    digitalWrite(ledR, 1);
+    digitalWrite(ledG, 0);
+    digitalWrite(ledB, 0);
+    parar();
+    if(pelota == true){
+      estado = 1;
+      recepcion = false;
+      parar();
+    }
+  break;
+  case 1: 
+  
     // Si el led está activo, ejecuta el código siguiente
     switch (estado) {
-      case :
-        if (digitalwrite(ENABLE) == 0) {
-          if (digitalwrite(B_IN) == 0) {
-            estado = 0;
-            
-          } else {
-            estado = 1;
-          
-          }
-        } else {
-          estado = 0;
-      
-        }
-        digitalWrite(LED_V, LOW);
-        verEstado(estado);
-        delay(ESPERA);
-        break;
-      case 1:
-        if (digitalRead(ENABLE) == 0) {
-          if (digitalRead(B_IN) == 0) {
-            estado = 2;
-           
-          } else {
-            estado = 1;
-          
-          }
-        } else {
-          estado = 1;
-          verSecuencia(0b0001);
-        }
-        digitalWrite(LED_V, LOW);
-        verEstado(estado);
-        delay(ESPERA);
-        break;
-
-      case 2:
-      if (digitalRead(ENABLE) == 0) {
-        if (digitalRead(B_IN) == 0) {
-          estado = 0;
-          
-        } else {
-          estado = 3;
-         
-        }
-      } else {
+       if  ( digitalWrite(ledR, 1);  )
+        pelota=true;
         estado = 2;
-        verSecuencia(0b0010);
-      }
-      digitalWrite(LED_V, LOW);
-      verEstado(estado);
-      delay(ESPERA);
-      break;
-
-
-      case 3:
-        if (digitalRead(ENABLE) == 0) {
-          if (digitalRead(B_IN) == 0) {
-            estado = 2;
-            
-          } else {
-            estado = 4;
-            
-          }
-        } else {
-          estado = 3;
-          
-        }
-        digitalWrite(LED_V, HIGH);
+            }
+        } else { digitalWrite(ledR, 0););
         verEstado(estado);
         delay(ESPERA);
         break;
-      case 4:
-        // Lógica para el estado 4
-        digitalWrite(LED_V, LOW);
-        // Hacer sonar la alarma 4 veces
-        for (int i = 0; i < 4; i++) {
-          digitalWrite(BUZZER, HIGH);
-          delay(500); // Sonido de la alarma durante 500 ms
-          digitalWrite(BUZZER, LOW);
-          delay(500); // Espera de 500 ms entre los sonidos
+          estado = 0;
         }
-        alarmCount = 4; // Establecer el contador en 4 para indicar que la alarma ha sonado 4 veces
-        digitalWrite(LED_V, HIGH);
-        delay(1000);
-        // Cambiar a estado 0
-        estado = 0;
-     
-        break;
-      case 6:
-        // Lógica para el caso 6 (error)
-        // Aquí puedes agregar cualquier lógica específica para el caso 6
-        break;
-    }
+       
+      case 2:
+        if{estado=2){
+          
+      void ejecucionTrayectoria;
+          digitalWrite(ledR, 1);
+    digitalWrite(ledG, 0);
+    digitalWrite(ledB, 0);
+        }  
+   
+     else{
+       
+     }     
+       
+          }}
+      
+      
+  
   }
-}
